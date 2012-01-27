6167ae3e03baab099a2fff77fede51dc35c51f8a
Ian Onesmus Mukewa <e-nesmukewa@hotmail.com>
2012-01-27 10:18:27 +0300

    Cleaning up of the project

--- index.php
+++ index.php
@@ -6,16 +6,17 @@ $authController = new AuthController();
 
 if (!isset($_SESSION['user_id'])) {
 	//Not logged in, send to login page.
-	header('Location: index.html');
+	header('Location: auth/login.php');
 } else {
 	//Check we have the right user
-	$logged_in = $authController->checkSession();
-	if(empty($logged_in)){
+	$session = $authController->checkSession();
+	if(!$logged_in){
 		//Bad session, ask to login
-		$auth->logout();
-		header( 'Location: index.html' );
+		$authController->logout();
+		header( 'Location: auth/login.php' );
 	} else {
 		//User is logged in, show the page
+		echo "<b>Am logged in</b>";
 	}
 }
 
